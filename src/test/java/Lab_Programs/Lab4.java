4ffdd6866e893f908e9fef52076e44628f80c065
Jaikrishnaa <jaikrishnaa.git@gmail.com>
2025-08-26 16:14:01 +0530

    Commit 2 Aug26

--- src/test/java/Lab_Programs/Lab4.java
+++ src/test/java/Lab_Programs/Lab4.java
@@ -1,51 +1,77 @@
 package Lab_Programs;
 
 import org.testng.annotations.Test;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.BeforeClass;
+
 import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
-public class Lab4 {
-  WebDriver driver;
+public class Lab4{
+	WebDriver driver;
   @Test(dataProvider = "dp")
   public void f(Integer n, String s) throws InterruptedException {
-	  driver.findElement(By.linkText("Desktops")).click();
-	  driver.findElement(By.linkText("Mac (1)")).click();
-	  Select drop = new Select(driver.findElement(By.id("input-sort")));
-	  drop.selectByVisibleText("Name (Z - A)");
-	  driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div/div[2]/div[2]/button[1]")).click();
-	  driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("Mobile");
-	  driver.findElement(By.xpath("/html/body/header/div/div/div[2]/div/span/button")).click();
-	  driver.findElement(By.xpath("//input[@id='input-search']")).clear();
-	  driver.findElement(By.xpath("//input[@id='description']")).click();
+	  System.out.println("This is the test");
 	  
-	  
+	  driver.get("http://tutorialsninja.com/demo/");
+
+      Assert.assertEquals(driver.getTitle(), "Your Store", "Title mismatch!");
+
+      driver.findElement(By.linkText("Desktops")).click();
+
+      driver.findElement(By.linkText("Mac (1)")).click();
+
+      WebElement sortBy = driver.findElement(By.id("input-sort"));
+      sortBy.sendKeys("Name (A - Z)");
+
+      WebElement heading = driver.findElement(By.xpath("//h2[contains(text(),'Mac')]"));
+      Assert.assertTrue(heading.isDisplayed(), "Mac heading not found!");
+      Thread.sleep(2000);
+
+      driver.findElement(By.xpath("//button[contains(@onclick,'cart.add')]")).click();
+
+      WebElement searchBox = driver.findElement(By.name("search"));
+      searchBox.sendKeys("Mobile");
+      driver.findElement(By.cssSelector("button.btn.btn-default")).click();
+      //Thread.sleep(2000);
+
+      WebElement searchCriteria = driver.findElement(By.name("search"));
+      searchCriteria.clear();
+
+      searchCriteria.sendKeys("Monitors");
+      driver.findElement(By.name("description")).click();
+      driver.findElement(By.cssSelector("button.btn.btn-default")).click();
+      //Thread.sleep(2000);
   }
   @BeforeMethod
   public void beforeMethod() {
-	  WebDriverManager.firefoxdriver().setup();
-	  driver = new FirefoxDriver();
-	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	  driver.get("https://tutorialsninja.com/demo/index.php?");
-	  driver.manage().window().maximize();
-	  
+	  System.out.println("This is before method");
+	  WebDriverManager.chromedriver().setup();
+		 driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
   }
 
   @AfterMethod
   public void afterMethod() {
+	  System.out.println("This is after method");
 	  driver.quit();
+
   }
 
 
@@ -58,26 +84,38 @@ public class Lab4 {
   }
   @BeforeClass
   public void beforeClass() {
+	  System.out.println("This is before class");
+
   }
 
   @AfterClass
   public void afterClass() {
+	  System.out.println("This is after class");
+
   }
 
   @BeforeTest
   public void beforeTest() {
+	  System.out.println("This is before test");
+
   }
 
   @AfterTest
   public void afterTest() {
+	  System.out.println("This is after test");
+
   }
 
   @BeforeSuite
   public void beforeSuite() {
+	  System.out.println("This is before suite");
+
   }
 
   @AfterSuite
   public void afterSuite() {
+	  System.out.println("This is after suite");
+
   }
 
 }
---
+++ src/test/java/Lab_Programs/Lab9_part2_lab4.java
@@ -0,0 +1,97 @@
+package Lab_Programs;
+
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.BeforeClass;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class Lab9_part2_lab4 {
+  WebDriver driver;
+  @Test(dataProvider = "dp")
+  public void f(Integer n, String s) throws InterruptedException {
+	  
+	  driver.get("https://tutorialsninja.com/demo/index.php?");
+	  driver.findElement(By.linkText("Desktops")).click();
+	  driver.findElement(By.linkText("Mac (1)")).click();
+	  Select drop = new Select(driver.findElement(By.id("input-sort")));
+	  drop.selectByVisibleText("Name (Z - A)");
+	  driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div/div[2]/div[2]/button[1]")).click();
+	  driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("Mobile");
+	  driver.findElement(By.xpath("/html/body/header/div/div/div[2]/div/span/button")).click();
+	  driver.findElement(By.xpath("//input[@id='input-search']")).clear();
+	  driver.findElement(By.xpath("//input[@id='description']")).click();
+	  
+	  
+  }
+  @Parameters("browser")
+  @BeforeMethod
+  public void beforeMethod(String brow) {
+	  if(brow.equalsIgnoreCase("chrome"))
+	  {
+	  WebDriverManager.chromedriver().setup();
+		 driver=new ChromeDriver();
+	  }
+	  
+	  if(brow.equalsIgnoreCase("edge"))
+	  {
+	  WebDriverManager.edgedriver().setup();
+		 driver=new EdgeDriver();
+	  }
+	  
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	  
+	  
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  driver.quit();
+  }
+
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a" },
+      new Object[] { 2, "b" },
+    };
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
